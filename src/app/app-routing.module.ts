f1176ded73cc63b975963a2cdbbb408fb482b0d5
RolandLM <rolando.loaisiga21908138@estu.unan.edu.ni>
2024-03-19 21:59:10 -0600

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { RouterModule, Routes } from "@angular/router"; 
-
-import { AppComponent } from "./app.component";
+import { LoginComponent } from "./View/Account/login/login.component";
+import { ProfileComponent } from "./View/Account/Profile/profile.component";
+import { HomeComponent } from "./View/Home/home.component";
 
 const routes: Routes = [
-  { path: '', component: AppComponent }
+  { path: '', redirectTo: '/Home', pathMatch: 'full' },
+  { path: 'Home', component: HomeComponent },
+  { path: 'Login', component: LoginComponent },
+  { path: 'Profile', component: ProfileComponent}
 ];
 
 export const AppRoutingModule = RouterModule.forRoot(routes, {useHash: true});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
-import{ AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+
+//componentes
 import { LoginComponent } from './View/Account/login/login.component';
+import { ProfileComponent } from './View/Account/Profile/profile.component';
+import { HomeComponent } from './View/Home/home.component';
 @NgModule({
-  declarations: [ AppComponent, LoginComponent ],
-  imports: [
-    BrowserModule,
-    CommonModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent, ProfileComponent, HomeComponent],
+  imports: [BrowserModule, CommonModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
